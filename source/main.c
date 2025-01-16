b6f620220e1ebb9da65cabeddafdc5880218249d
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2025-01-16 14:19:19 +0100

    Add files via upload

---
+++ source/main.c
@@ -0,0 +1,57 @@
+#include <windows.h>
+#include <stdlib.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	const char CLASS_NAME[] = "MainWnd";
+	
+	WNDCLASS wc = { };
+	
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	
+	RegisterClass(&wc);
+	
+	HWND hwnd = CreateWindowEx(
+		0,
+		CLASS_NAME,
+		"BatteryMeter - Status",	// TITLE
+		WS_OVERLAPPEDWINDOW,
+		
+		CW_USEDEFAULT, CW_USEDEFAULT, // window Position on the desktop
+		300, 430,		// Window Size | X , Y
+		
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+	
+	if(hwnd == NULL){	return 0;	}
+	
+	ShowWindow(hwnd, nCmdShow);
+	
+	MSG msg = {};
+	while(GetMessage(&msg, NULL, 0, 0) > 0){
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+	switch(uMsg){
+		
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+			
+		
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+	
