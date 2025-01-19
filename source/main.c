69aec32f43976736a5f4d99159dcf7c0b3e8dc10
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2025-01-19 17:57:46 +0100

    add other (but not checked) files

--- source/main.c
+++ source/main.c
@@ -1,47 +1,11 @@
 #include <windows.h>
 #include <stdlib.h>
+#include "resource.h"
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+//// Definitions
+HWND hText;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	const char CLASS_NAME[] = "MainWnd";
-	
-	WNDCLASS wc = { };
-	
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-	
-	RegisterClass(&wc);
-	
-	HWND hwnd = CreateWindowEx(
-		0,
-		CLASS_NAME,
-		"BatteryMeter - Status",	// TITLE
-		WS_OVERLAPPEDWINDOW,
-		
-		CW_USEDEFAULT, CW_USEDEFAULT, // window Position on the desktop
-		300, 430,		// Window Size | X , Y
-		
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-		);
-	
-	if(hwnd == NULL){	return 0;	}
-	
-	ShowWindow(hwnd, nCmdShow);
-	
-	MSG msg = {};
-	while(GetMessage(&msg, NULL, 0, 0) > 0){
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return 0;
-}
+/// end definitions
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
 	switch(uMsg){
@@ -49,9 +13,62 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
 		case WM_DESTROY:
 			PostQuitMessage(0);
 			return 0;
+			break;
+		case WM_CREATE:
+			hText = CreateWindowExW(
+				0,
+				L"STATIC",
+				L"Loading Window Procedures ...",
+				60, 215,
+				100, 30,
+				hwnd,NULL, NULL, NULL);
 			
-		
+			break;
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return 0;
 }
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	const wchar_t CLASS_NAME[] = L"MainWnd";
+	
+	WNDCLASSW wc = { };
+	
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	
+	if(!RegisterClassW(&wc)) {
+        MessageBoxW(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+	
+	HWND hwnd = CreateWindowExW(
+		0,
+		CLASS_NAME,
+		L"BatteryMeter - Status",	// TITLE
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT, // window Position on the desktop
+		300, 430,		// Window Size | X , Y
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+	
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	
+	MSG msg = {};
+	while(GetMessage(&msg, NULL, 0, 0) > 0){
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+
 	
---
+++ source/resource.h
@@ -0,0 +1,10 @@
+//// Language resource for main.c
+
+
+
+#define LOADING_WINPROC 1001
+#define WELCOME_MSG 1002
+
+#define TITLE_NOJOB 1003
+#define TITLE_STATUS 1004
+#define TITLE_JOBFAIL 1005
