efba656aa0bbafdf1da578262044a5793073acba
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2025-01-20 15:20:55 +0100

    add definitons etc.

--- source/main.c
+++ source/main.c
@@ -4,6 +4,9 @@
 
 //// Definitions
 HWND hText;
+HWND hFont;
+
+wchar_t szBuffer[2048];
 
 /// end definitions
 
@@ -15,13 +18,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
 			return 0;
 			break;
 		case WM_CREATE:
+			LoadStringW(GetModuleHandleW(NULL), LOADING_WINPROC, szBuffer, sizeof(szBuffer) / sizeof(wchar_t));
+			
 			hText = CreateWindowExW(
 				0,
 				L"STATIC",
-				L"Loading Window Procedures ...",
+				szBuffer,
 				WS_VISIBLE | WS_CHILD,	// DWstyles
-				60, 215,
-				100, 30,
+				20, 20,
+				210, 30,
 				hwnd,NULL, NULL, NULL);
 			
 			break;
@@ -40,17 +45,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = CLASS_NAME;
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	
 	if(!RegisterClassW(&wc)) {
-        MessageBoxW(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
+        	MessageBoxW(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+        	return 0;
+    	}
+
+	LoadStringW(hInstance, TITLE_NOJOB, szBuffer, sizeof(szBuffer) / sizeof(wchar_t));
 	
 	HWND hwnd = CreateWindowExW(
 		0,
 		CLASS_NAME,
-		L"BatteryMeter - Status",	// TITLE
+		szBuffer,	// TITLE
 		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
 		CW_USEDEFAULT, CW_USEDEFAULT, // window Position on the desktop
 		300, 430,		// Window Size | X , Y
