bff8303f934486b92e37d5101e670b3d1d3381e1
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2025-01-20 14:38:36 +0100

    Update ResFunc.c

--- source/res/ResFunc.c
+++ source/res/ResFunc.c
@@ -8,16 +8,17 @@
 // Checked the syntax - check when testing
 
 wchar_t* GetResourceStringW(HINSTANCE hInst, int strID){
-	static wchar_t Buffer[2048];
-	
-	LoadStringW(hInst, strID, Buffer, sizeof(Buffer) / sizeof(wchar_t));
-	return Buffer;
+    static wchar_t Buffer[2048];
+    if(LoadStringW(hInst, strID, Buffer, sizeof(Buffer) / sizeof(wchar_t)) == 0) {
+        MessageBoxW(NULL, L"Failed to load string resource", L"Error", MB_ICONERROR);
+    }
+    return Buffer;
 }
 
-// Checked the syntax - check when testing
 char* GetResourceStringA(HINSTANCE hInst, int strID){
-	static char Buffer[2048];
-	
-	LoadStringA(hInst, strID, Buffer, sizeof(Buffer) / sizeof(Buffer[0]));
-	return Buffer;
+    static char Buffer[2048];
+    if(LoadStringA(hInst, strID, Buffer, sizeof(Buffer) / sizeof(Buffer[0])) == 0) {
+        MessageBoxA(NULL, "Failed to load string resource", "Error", MB_ICONERROR);
+    }
+    return Buffer;
 }
