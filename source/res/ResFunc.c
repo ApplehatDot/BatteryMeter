be435cf1d612f75ee83c77924696d9a3738519b1
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2025-01-20 11:36:38 +0100

    Fix returned values
    define functions that return wchar_t* or char*

--- source/res/ResFunc.c
+++ source/res/ResFunc.c
@@ -5,17 +5,18 @@
 #include <wchar.h>
 #include <windows.h>
 
-// Unsure for now - Check the functionality later.
-wchar_t GetResourceStringW(HINSTANCE hInst, int strID, int BuffSize){
-	static wchar_t Buffer[BuffSize];
+// Checked the syntax - check when testing
+
+wchar_t* GetResourceStringW(HINSTANCE hInst, int strID){
+	static wchar_t Buffer[2048];
 	
 	LoadStringW(hInst, strID, Buffer, sizeof(Buffer) / sizeof(wchar_t));
 	return Buffer;
 }
 
-// Same as with Wide-char one - Check the functionality later.
-char GetResourceStringA(HINSTANCE hInst, int strID, int BuffSize){
-	static char Buffer[BuffSize];
+// Checked the syntax - check when testing
+char* GetResourceStringA(HINSTANCE hInst, int strID){
+	static char Buffer[2048];
 	
 	LoadStringA(hInst, strID, Buffer, sizeof(Buffer) / sizeof(Buffer[0]));
 	return Buffer;
