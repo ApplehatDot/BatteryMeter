c3b362a230b11545e73ed22521f26cfda2f95e8b
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2025-05-04 11:35:50 +0200

    Update resource.h

--- source/resource.h
+++ source/resource.h
@@ -1,6 +1,19 @@
 //// Language resource for main.c
 
 
+#define IDI_BUGCHK 103
+#define BUGCHK 102
+
+#define MANAGER_MENU 1
+#define MGR_BATMEASURE 11
+#define MGR_LASTMEASURE 12
+#define MGR_EXIT 13
+#define MGR_EXITDESC 14
+
+#define BATTERY_MENU 2
+#define BAT_CSYSBAT 21
+
+
 
 #define LOADING_WINPROC 1001
 #define WELCOME_MSG 1002
@@ -8,3 +21,11 @@
 #define TITLE_NOJOB 1003
 #define TITLE_STATUS 1004
 #define TITLE_JOBFAIL 1005
+
+#define AC_CONN_STAT 1300
+
+// ------------- ERRORs ------------ 
+
+#define ERR_MISSING_FUNC 10000		// Err 1
+#define ERR_LOAD_DLL_FAILED 20000		// Err 2
+#define ERR_NO_SUCH_DLL_FUNC 30000	// Err 3
